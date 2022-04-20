d4c590a671b40c4625ed19a062d9d974ddfd22c7
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-04-20 12:14:42 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "@/components/HeaderComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderComponent,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  props: {},
+};
+</script>
+
+<style scoped lang="scss">
+</style>
