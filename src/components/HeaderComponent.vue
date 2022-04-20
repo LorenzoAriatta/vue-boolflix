46f77b75ee7357b2bb602b3f0b31a5b01ae577ff
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-04-20 13:37:26 +0200

    aggiunta function api

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class=""></div>
+  <div class="container">
+    <div class="row justify-content-between align-items-center">
+      <div class="col-2"><h1 class="col-2">BOOFLIX</h1></div>
+      <div class="col-4">
+        <input type="text" placeholder="Search" />
+        <button type="submit" class="btn-primary">Cerca</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HeaderComponent",
   props: {},
+  methods: {
+    textToSearch() {
+      this.$emit("search");
+    },
+  },
 };
 </script>
 
